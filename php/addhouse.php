3e6faafbdfb96ab3f8e82a35a999d885b1d3cdf0
sheilaz <zst_2012@sina.com>
2018-07-19 15:49:13 +0800

    实现提交房源到数据库

---
+++ php/addhouse.php
@@ -0,0 +1,26 @@
+﻿<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "fz";
+
+error_reporting(0);
+
+// 创建连接
+$conn = new mysqli($servername, $username, $password, $dbname);
+// 检测连接
+if ($conn->connect_error) {
+    die("连接失败: " . $conn->connect_error);
+} 
+
+$sql = "INSERT INTO house (loupan, huxing, yangtai, chaoxiang, jiaoyifangshi, jiage, mianji, louhao, cenghao, fanghao)
+VALUES ('{$_POST['district']}', '{$_POST['style']}', '{$_POST['balcony']}', '{$_POST['direction']}', '{$_POST['pay']}', '{$_POST['price']}', '{$_POST['area']}', '{$_POST['place01']}', '{$_POST['place02']}', '{$_POST['place03']}')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "新记录插入成功";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>

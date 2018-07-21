9b1b77b0bf375ffcda8a2f27e4f103381c10ee41
sheilaz <zst_2012@sina.com>
2018-07-21 15:17:38 +0800

    售房页面

--- php/addhouse.php
+++ php/addhouse.php
@@ -1,26 +1,96 @@
 ﻿<?php
 $servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "fz";
+$username = "webuser";
+$password = "admin123";
+$dbname = "webuser";
+$building = time();
 
-error_reporting(0);
-
-// 创建连接
 $conn = new mysqli($servername, $username, $password, $dbname);
-// 检测连接
-if ($conn->connect_error) {
-    die("连接失败: " . $conn->connect_error);
-} 
 
-$sql = "INSERT INTO house (loupan, huxing, yangtai, chaoxiang, jiaoyifangshi, jiage, mianji, louhao, cenghao, fanghao)
-VALUES ('{$_POST['district']}', '{$_POST['style']}', '{$_POST['balcony']}', '{$_POST['direction']}', '{$_POST['pay']}', '{$_POST['price']}', '{$_POST['area']}', '{$_POST['place01']}', '{$_POST['place02']}', '{$_POST['place03']}')";
+$conn->query("set character set 'utf8'"); 
+$conn->query("set names 'utf8'");
+ 
+$sql = "
+	INSERT INTO house (building, district, style, balcony, direction, pay, price, area, bn, fn, rn)
+	VALUES ('{$building}', '{$_POST['district']}', '{$_POST['style']}', '{$_POST['balcony']}', '{$_POST['direction']}', '{$_POST['pay']}', '{$_POST['price']}', '{$_POST['area']}', '{$_POST['place01']}', '{$_POST['place02']}', '{$_POST['place03']}')
+	
+	";
+$conn->query($sql);
 
-if ($conn->query($sql) === TRUE) {
-    echo "新记录插入成功";
-} else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
+if($_FILES) {
+	foreach($_FILES['overall']['type'] as $key=>$value) {
+		$type = 'overall';
+        switch ($value) {
+            case 'image/jpeg': $ext = 'jpg';
+                break;
+            case 'image/png': $ext = 'png';
+                break;
+            case 'image/gif': $ext = 'gif';
+            default:
+                $ext = '';
+                break;
+        }
+        if($ext) {
+            $name = 'upload/'.time().$type.'_'."$key.$ext";
+            move_uploaded_file($_FILES['overall']['tmp_name'][$key], $name);
+			$name = 'php/'.$name;
+			$sql = "
+			UPDATE house SET image = '{$name}' WHERE building = '{$building}'
+			";
+			$conn->query($sql);
+        }
+		
+    }
+	foreach($_FILES['external']['type'] as $key=>$value) {
+		$type = 'external';
+        switch ($value) {
+            case 'image/jpeg': $ext = 'jpg';
+                break;
+            case 'image/png': $ext = 'png';
+                break;
+            case 'image/gif': $ext = 'gif';
+            default:
+                $ext = '';
+                break;
+        }
+        if($ext) {
+            $name = 'upload/'.time().$type.'_'."$key.$ext";
+            move_uploaded_file($_FILES['external']['tmp_name'][$key], $name);
+			$name = 'php/'.$name;
+			$sql = "
+			INSERT INTO image (building, type, url)
+			VALUES('{$building}', 'external', '{$name}')
+			";
+			$conn->query($sql);
+        }
+		
+    }
+	foreach($_FILES['internal']['type'] as $key=>$value) {
+		$type = 'internal';
+        switch ($value) {
+            case 'image/jpeg': $ext = 'jpg';
+                break;
+            case 'image/png': $ext = 'png';
+                break;
+            case 'image/gif': $ext = 'gif';
+            default:
+                $ext = '';
+                break;
+        }
+        if($ext) {
+            $name = 'upload/'.time().$type.'_'."$key.$ext";
+            move_uploaded_file($_FILES['internal']['tmp_name'][$key], $name);
+			$name = 'php/'.$name;
+			$sql = "
+			INSERT INTO image (building, type, url)
+			VALUES('{$building}', 'internal','{$name}')
+			";
+			$conn->query($sql);
+        }
+		
+    }
+
+	
 }
-
 $conn->close();
 ?>
